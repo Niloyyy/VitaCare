9ebf8eca3d9f3d31896cd30adf23209c10eceae7
Niloyyy <niloydeb48@gmail.com>
2025-07-21 22:47:38 +0600

    added donor info storing in database feature

--- app/src/main/java/com/example/vitacare_app_250/BecomeADonorActivity.java
+++ app/src/main/java/com/example/vitacare_app_250/BecomeADonorActivity.java
@@ -1,13 +1,50 @@
 package com.example.vitacare_app_250;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class BecomeADonorActivity extends AppCompatActivity {
+
+    private EditText nameInput, contactNumber, bloodGroup;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_become_a_donor);
+
+        nameInput = findViewById(R.id.Name);
+        contactNumber = findViewById(R.id.Phone);
+        bloodGroup = findViewById(R.id.BloodGrp);
+
+        submitButton = findViewById(R.id.submitBtn);
+
+        submitButton.setOnClickListener(v -> {
+            String name = nameInput.getText().toString().trim();
+            String number = contactNumber.getText().toString().trim();
+            String bloodGrp = bloodGroup.getText().toString().trim().toUpperCase();
+
+            List<String> validBloodGroups = Arrays.asList("A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-");
+
+            if (!validBloodGroups.contains(bloodGrp)) {
+                Toast.makeText(this, "Invalid blood group! Please enter a valid one.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            BloodDonor donor = new BloodDonor(name, number, bloodGrp);
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("blood_donors");
+            ref.push().setValue(donor).addOnSuccessListener(aVoid -> Toast.makeText(this, "Donor Added!", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+        });
     }
 }
---
+++ app/src/main/java/com/example/vitacare_app_250/BloodDonor.java
@@ -0,0 +1,14 @@
+package com.example.vitacare_app_250;
+
+public class BloodDonor {
+    public String Name , Phone , BloodGroup;
+    public BloodDonor()
+    {
+
+    }
+    public BloodDonor(String Name , String Phone , String BloodGroup){
+        this.Name = Name;
+        this.Phone = Phone;
+        this.BloodGroup = BloodGroup;
+    }
+}
