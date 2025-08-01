714f08f7547b69e0e858ec1b4e4251c38f067a9a
diippu <dipudeb883@gmail.com>
2025-08-02 02:40:11 +0600

    Add HospitalAdapter and item_hospital layout

--- app/src/main/java/com/example/vitacare_app_250/HospitalAdapter.java
+++ app/src/main/java/com/example/vitacare_app_250/HospitalAdapter.java
@@ -1,4 +1,39 @@
 package com.example.vitacare_app_250;
 
-public class HospitalAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class HospitalAdapter extends ArrayAdapter<Hospital> {
+    private final Context context;
+    private final List<Hospital> hospitals;
+
+    public HospitalAdapter(Context context, List<Hospital> hospitals) {
+        super(context, 0, hospitals);
+        this.context = context;
+        this.hospitals = hospitals;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        Hospital hospital = hospitals.get(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_hospital, parent, false);
+        }
+
+        ((TextView) convertView.findViewById(R.id.tvName)).setText(hospital.name);
+        ((TextView) convertView.findViewById(R.id.tvAddress)).setText(hospital.address);
+        ((TextView) convertView.findViewById(R.id.tvBeds)).setText("Beds: " + hospital.available_beds);
+        ((TextView) convertView.findViewById(R.id.tvIcu)).setText("ICU: " + hospital.available_icu);
+
+        return convertView;
+    }
 }
